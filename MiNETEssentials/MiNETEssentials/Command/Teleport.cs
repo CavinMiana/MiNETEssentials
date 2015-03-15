15f88837b21c4fc727881befcd673953c0d075dd
FDKPIBC <yt185110754@gmail.com>
2015-03-15 22:27:40 +0800

    oops

---
+++ MiNETEssentials/MiNETEssentials/Command/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MiNET;
+using MiNET.Net;
+using MiNET.Utils;
+using MiNET.Worlds;
+using MiNET.Plugins;
+using MiNETEssentials;
+using MiNET.Plugins.Attributes;
+using MiNETCNServer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiNETEssentials.Command
+{
+    //Easy player to player teleports, including teleport requests.  
+    class Teleport : Plugin
+    {
+
+    }
+}
